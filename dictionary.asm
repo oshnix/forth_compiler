b617445b5a2144e1596ed9dfc9fff7e96243ae31
oshnix <sdsdaniil@gmail.com>
2016-09-21 11:56:35 +0300

    Compilator was added.

--- dictionary.asm
+++ dictionary.asm
@@ -21,15 +21,45 @@ find_word:
 	mov rax, rsi
 	pop rsi
 	ret
-	
+
+cfa:
+	xor rax, rax
+	add rdi, word_size
+.loop:
+	mov al, [rdi]
+	inc rdi
+	test al, al
+	jz .ret
+	jmp .loop
+.ret:
+	inc rdi
+	mov rax, rdi
+	ret
+
+
+docol:
+	add w, word_size
+	mov rax, [w]
+	call [rax]
+	jmp docol
+
+put:
+	add w, word_size
+	mov rax, [w]
+	jmp push_num
+
 
 native '+', plus
+	mov rdi, 2 * data_size
+	call underflow_check
 	sub data_top, data_size
 	mov r8, [data_top]
 	add [data_top - data_size], r8
 	ret		
 
 native '-', minus
+	mov rdi, 2 * data_size
+	call underflow_check
 	sub data_top, data_size
 	mov r8, [data_top]
 	sub [data_top - data_size], r8
@@ -51,6 +81,8 @@ native '.s', print
 	ret
 
 native '*', mul
+	mov rdi, 2 * data_size
+	call underflow_check
 	sub data_top, data_size
 	mov rax, [data_top - data_size]
 	imul qword[data_top]
@@ -58,6 +90,8 @@ native '*', mul
 	ret
 
 native '/', div
+	mov rdi, 2 * data_size
+	call underflow_check
 	sub data_top, data_size
 	mov rax, [data_top - data_size]
 	idiv qword[data_top]
@@ -65,17 +99,57 @@ native '/', div
 	ret
 
 native '<', less
+	mov rdi, 2 * data_size
+	call underflow_check
 	sub data_top, 2 * data_size
 	mov rax, [data_top]
 	mov qword[data_top], 0
 	add data_top, data_size
 	cmp rax, [data_top]
-	jz .return
+	jae .return
 	inc qword[data_top - data_size]
 	.return: ret
 
+native ':', begin,'I'
+	mov byte[state], 1
+	mov rdi, [last_word]
+	mov [here], rdi
+	mov qword[last_word], here
+	add here, word_size
+	call read_word
+	mov rdi, rax
+	mov rsi, here
+	call string_copy
+	mov here, rsi
+	inc here
+	mov qword[here], docol
+	add here, word_size
+	jmp compilator
+
+
+
+native ';', end,'I'
+	mov byte[state], 0
+	mov qword[here], xt_end_word
+	add here, word_size
+	jmp compilator
+
+
+
+end_word:
+	pop rax
+	ret
+
+
+
+native 'mem', mem
+	mov qword[data_top], userdata
+	add data_top, data_size
+	ret
 
 native '=', equal
+	mov rdi, 2 * data_size
+	call underflow_check
 	sub data_top, 2 * data_size
 	mov rax, [data_top]
 	mov qword[data_top], 0
@@ -86,17 +160,23 @@ native '=', equal
 	.ret: ret
 
 native 'dup', dup
+	mov rdi, data_size
+	call underflow_check
 	mov rax, [data_top - data_size]
 	mov [data_top], rax
 	add data_top, data_size
 	ret
 
 native '.', pop
+	mov rdi, data_size
+	call underflow_check
 	sub data_top, data_size
 	mov rdi, [data_top]
 	jmp print_int
 
 native 'rot', rotate
+	mov rdi, 3 * data_size
+	call underflow_check
 	sub data_top, 3 * data_size
 	mov rax, [data_top]
 	add data_top, data_size
@@ -110,6 +190,8 @@ native 'rot', rotate
 	ret
 
 native 'swap', swap
+	mov rdi, 2 * data_size
+	call underflow_check
 	mov rax, [data_top - data_size]
 	mov rdx, [data_top - 2 * data_size]
 	mov [data_top - 2* data_size], rax
@@ -117,11 +199,15 @@ native 'swap', swap
 	ret
 
 native 'drop', drop
+	mov rdi, data_size
+	call underflow_check
 	sub data_top, data_size
 	ret
 
 
 native 'and', and
+	mov rdi, 2 * data_size
+	call underflow_check
 	sub data_top, data_size
 	mov rax, [data_top]
 	and [data_top - data_size],rax 
@@ -129,6 +215,8 @@ native 'and', and
 
 
 native 'not', not
+	mov rdi, data_size
+	call underflow_check
 	mov rax, [data_top - data_size]
 	test rax, rax
 	jz .zero
@@ -147,16 +235,51 @@ native 'key', key
 	ret
 
 native 'emit', keyout
+	mov rdi, data_size
+	call underflow_check
 	sub data_top, data_size
 	mov rdi, [data_top]
 	jmp print_char
 
+input:
+	call read_word
+	mov rdi, word_buffer
+	call parse_int
+	ret	
+
+native 'number', number
+	call input
+	test rdx, rdx
+	jnz .finish
+	jmp error
+.finish:
+	mov rdi, rax
+push_num:
+	mov [data_top], rax
+	add data_top, data_size
+	ret
 
 native 'exit', exit
 	mov rax, 60
 	xor rdi, rdi
 	syscall	
 
+colon '>', greater
+	dq xt_swap
+	dq xt_less
+	dq xt_end_word
+
+
+underflow_check:
+	mov rax, data_top
+	sub rax, stack_data
+	sub rax, rdi
+	jns .ret
+	pop rax
+	mov rdi, err2
+	jmp print_string
+	.ret: ret
 
 section .data
-last_word: dq link	
+last_word: dq link
+xt_end_word: dq end_word	
--- forth.asm
+++ forth.asm
@@ -1,15 +1,21 @@
 %define data_top rbx
+%define return_top rbp
+%define here r13
+%define w r14
 %define data_size 8
 
 section .data
+state: db 0
 stack_data:
 	times 65535 db 0
 data: db 0
 stack_return:
 	times 65535 db 0
 return db 0
-err_mes: db 'NO_COMMAND!', 0
-userdata: db 0
+dictionary: times 65536 db 0
+err1: db 'wrong input!', 0
+err2: db 'Not enough opperands', 0
+userdata: times 65536 db 0
 section .text
 
 %include 'lib.inc'
@@ -20,28 +26,59 @@ section .text
 global _start
 _start:
 	mov data_top, stack_data
-.loop:
-	call read_word
-	mov rdi, rax
-	call parse_int
+	mov here, dictionary
+interpretator:
+	call input
 	test rdx, rdx
 	jz .word
-	mov [data_top], rax
-	add data_top, data_size
-	jmp .loop
+	call push_num
+	jmp interpretator
 .word:
 	mov rdi, word_buffer 
 	call lower_case
 	mov rdi, word_buffer
 	call find_word
 	test rax, rax
-	jz .error
+	jz error
 	inc rax
-	call [rax]
-	jmp .loop
-.error:
-	mov rdi, err_mes
+	mov w, rax
+	call [w]
+	jmp interpretator 
+
+error:
+	mov rdi, err1
 	call print_string
-	jmp .loop	
+	jmp interpretator	
 	
 
+compilator:
+.loop:
+	mov al, byte[state]
+	test al, al
+	jz interpretator
+	call input
+	test rdx, rdx
+	jz .word
+	mov qword[here], put
+	add here, word_size
+	mov qword[here], rax	
+	add here, word_size
+.word:	
+	mov rdi, word_buffer
+	call lower_case
+	mov rdi, word_buffer
+	call find_word
+	test rax, rax
+	jz error
+	mov dil, byte[rax]
+	inc rax
+	test dil, dil
+	jnz .int 
+	mov qword[here], rax
+	add here, word_size
+	jmp .loop
+.int:
+	call [rax]
+		
+	
+	
